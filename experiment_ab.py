cd364284d8295e9e9377ccda25b0840f12d953d2
LukaszJ <Jaremek.Lukasz@gmail.com>
2023-06-02 22:46:48 +0200

    create shit model

---
+++ experiment_ab.py
@@ -0,0 +1,11 @@
+from random import randint
+
+
+def run_ab(
+        time_series_path: str,
+        parameters_path: str,
+        expected_results_path: str,
+        parameters_size: tuple[int, int] = (7, 24)
+        ) -> tuple[float, float]:
+
+    pass
